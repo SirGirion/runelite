a7ab9f52e5b67df7c9552e6cfac5a552219e884c
SirGirion <seallproducts@gmail.com>
2023-06-26 21:17:08 -0400

    Support regular gauntlet

--- runelite-client/src/main/java/net/runelite/client/plugins/gauntlet/GauntletProjectiles.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/gauntlet/GauntletProjectiles.java
@@ -2,17 +2,20 @@ package net.runelite.client.plugins.gauntlet;
 
 public class GauntletProjectiles
 {
-	private static final int HUNLLEF_RANGED = 1712;
-	private static final int HUNLLEF_MAGIC = 1708;
-	private static final int HUNLLEF_PRAYER_DISABLE = 1714;
+	private static final int HUNLLEF_RANGE = 1711;
+	private static final int CORRUPTED_HUNLLEF_RANGED = 1712;
+	private static final int HUNLLEF_MAGIC = 1707;
+	private static final int CORRUPTED_HUNLLEF_MAGIC = 1708;
+	private static final int HUNLLEF_PRAYER_DISABLE = 1713;
+	private static final int CORRUPTED_HUNLLEF_PRAYER_DISABLE = 1714;
 
 	protected static boolean isCountableRanged(final int id)
 	{
-		return id == HUNLLEF_RANGED || id == HUNLLEF_PRAYER_DISABLE;
+		return id == HUNLLEF_RANGE || id == CORRUPTED_HUNLLEF_RANGED;
 	}
 
 	protected static boolean isCountableMagic(final int id)
 	{
-		return id == HUNLLEF_MAGIC || id == HUNLLEF_PRAYER_DISABLE;
+		return id == HUNLLEF_MAGIC || id == HUNLLEF_PRAYER_DISABLE || id == CORRUPTED_HUNLLEF_MAGIC || id == CORRUPTED_HUNLLEF_PRAYER_DISABLE;
 	}
 }
